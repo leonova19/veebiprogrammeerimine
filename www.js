48f262e9be58d69056d13c1ff66f274f8f4cffea
Eliana Leonova <leonova@tlu.ee>
2023-10-03 20:26:11 +0300

    Kodune algatus

--- www.js
+++ www.js
@@ -2,6 +2,7 @@ const http = require("http");
 const path = require("path");
 const url = require("url");
 const fs = require("fs");
+const dateTime = require('./dateTime_et.js')
 const pageHead = '<!DOCTYPE html>\n<html>\n<head>\n\t<meta charset="utf-8">\n\t<title>Andrus Rinde, veebiprogrammeerimine 2023</title>\n</head>\n<body>';
 const pageBanner = '\n\t<img src="banner.png" alt="Lehe bänner">\n';
 const pageBody = '\n\t<h1>Andrus Rinde</h1>\n\t<p>See leht on loodud <a href="https://www.tlu.ee" target="_blank">TLÜ</a> Digitehnoloogiate instituudis õppetöö raames!</p>\n\t<p>Olen väga tore!</p>\n\t<p>Keskmist kasvu meesterahvas parimais aastais!</p>\n\t<hr>\n\t<p>Kursus, mille raames leht tehti on: veebiprogrammeerimine.</p>';
@@ -16,7 +17,10 @@ http.createServer(function(req, res){
 		res.write(pageHead);
 		res.write(pageBanner);
 		res.write(pageBody);
+		res.write(`<p>Lehe avamise hetkel oli ${dateTime.timeNowET()}</p>`);
 		res.write('<p><a href="addName">Lisame nime</a>!</p>');
+		res.write('<p><a href="semesterprogress">Semestri progress</a>!</p>');
+		res.write('<p><a href="pildid">Ülikooli pildid</a>!</p>');
 		res.write(pageFoot);
 		//et see kõik valmiks ja ära saadetaks
 		return res.end();
@@ -31,9 +35,33 @@ http.createServer(function(req, res){
 		//et see kõik valmiks ja ära saadetaks
 		return res.end();
 	}
+	else if (currentURL.pathname === "/semesterprogress"){ 
+		res.writeHead(200, {"Content-Type": "text/html"});
+		res.write(pageHead);
+		res.write(pageBanner);
+		res.write(pageBody);
+		res.write('<h2>SEMESTRI PROGRESS TEHA</h2>');
+		const max = 10;
+		const value = 5;
+		res.write(`<meter min="0" max="${max}" value="${value}"></meter>`)
+		res.write(pageFoot);
+		//et see kõik valmiks ja ära saadetaks
+		return res.end();
+	}
 	else if (currentURL.pathname === "/banner.png"){
-		console.log("tahan pilti!");
 		let filePath = path.join(__dirname, "public", "banner/banner.png");
+		fs.readFile(filePath, (err, data)=>{
+			if(err){
+				throw err;
+			}
+			else {
+				res.writeHead(200, {"Content-Type": "image/png"});
+				res.end(data);
+			}
+		});
+	} 
+	else if (currentURL.pathname === "/pildid"){
+		let filePath = path.join(__dirname, "public", "tluphotos/tlu_5.jpeg");
 		fs.readFile(filePath, (err, data)=>{
 			if(err){
 				throw err;
